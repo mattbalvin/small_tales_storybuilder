95d575d480eb7c5d9242a8dc5a3dd30781162d63
Matt Balvin <matt.balvin@gmail.com>
2025-06-29 22:47:54 -0700

    Updated LandingPage.svelte

--- src/lib/components/landing/LandingPage.svelte
+++ src/lib/components/landing/LandingPage.svelte
@@ -247,14 +247,12 @@
   <footer class="bg-[#558A80] text-white py-12 px-4">
     <div class="container">
       <div class="flex justify-between items-center mb-8">
-        <div>        
-          <div class="bg-[#FEF5E1] rounded-lg border-2 border-[#E2684F] p-2">
-            <img 
-              src="/a-vibrant-logo-illustration-of-an-open-s_cW_N4un2Sn2tPv27_2LPKA_ihURQ9I_SK6GaVnhM6m41Q.png" 
-              alt="Small Tales Logo" 
-              class="h-20"
-            />
-          </div>
+        <div class="bg-[#FEF5E1] rounded-lg border-2 border-[#E2684F] p-2">
+          <img 
+            src="/a-vibrant-logo-illustration-of-an-open-s_cW_N4un2Sn2tPv27_2LPKA_ihURQ9I_SK6GaVnhM6m41Q.png" 
+            alt="Small Tales Logo" 
+            class="h-20"
+          />
         </div>
 
         <!--div class="grid md:grid-cols-3 gap-8"-->
